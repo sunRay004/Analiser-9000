4c2ed830be8305800035c22a5f661bc56729c08f
Kirill Pavlovskiy <kirill.s.pavlovskiy@gmail.com>
2023-05-25 21:34:32 -0700

    spelling is better

--- regexes.py
+++ regexes.py
@@ -1,15 +1,17 @@
 import re
 
+
 def regex(exp):
     exp = input("lines containing ___ should be saved: ")
     return exp
-    
+   
 def useex(savex):
-    print("this may take betwen 10 sec to 621 hours \nno, the program has not crashed")
+    print("this may take between 10 sec to 621 hours \nno, the program has not crashed")
     paray = []
     with open("BrowserHistory.txt", "r", encoding='utf-8', errors='replace') as activeFile:
         allLines = []    
         allLines = activeFile.readlines()
+
 
     for line in allLines:
         if re.search(savex, line):
@@ -18,4 +20,7 @@ def useex(savex):
             f.write(line)
     activeFile.close()
 
+
     return paray
+
+
