cecea86abbc8706aafc0b5f56ddad7f63b85ec48
Kirill Pavlovskiy <kirill.s.pavlovskiy@gmail.com>
2023-05-25 21:35:00 -0700

    spelling

--- MainProgramLoop.py
+++ MainProgramLoop.py
@@ -8,23 +8,24 @@ saveExpression = r'"url":'
 WebArray = []
 ProssesedArray = []
 
-## this desides what to call and what to do
+
+## this decides what to call and what to do
 ## just a while loop
 running = True
 print("launched, type help to view commands")
 while running:
-    inp = input("imput: ")
-    
+    inp = input("input: ")
+   
     ## help printout
     if inp == "help" :
         print("------ regexes, use these first ------")
         print()
-        print("regex: lets you select what to save to the prossesed array")
-        print("useex: saves selected thing to the prossesed array (default saves website urls)")
+        print("regex: lets you select what to save to the processed array")
+        print("useex: saves selected thing to the processed array (default saves website urls)")
         print()
-        print("------ prossesing functions, use after regexes ------")
+        print("------ processing functions, use after regexes ------")
         print()
-        print("findNames: finds domain names, needs prossesed array, creates the web array")
+        print("findNames: finds domain names, needs processed array, creates the web array")
         print("count: counts websites, needs web array, creates export array")
         print("export: exports export array into a .txt file, for use in graphical analysis (use a spreadsheet lol, idk how to make python graphs)")
         print()
@@ -40,62 +41,71 @@ while running:
         print()
         print("clear: menu for clearing arrays")
         print("cleanWebArray: uses a regex expression to filter the web array")
-        print("TTA: contents of prossesed text to prossesed array (usefull if useex was run and the program reloaded, saves time)")
-        print("ATT: contents of prossesed array to prossesed text (does not clear prossesed text)")
-    
+        print("TTA: contents of processed text to processed array (usefull if useex was run and the program reloaded, saves time)")
+        print("ATT: contents of processed array to processed text (does not clear processed text)")
+   
     if inp == "regex" :
         saveExpression = regexes.regex(saveExpression)
         print("Done")
-        
+       
     if inp == "useex" :
         ProssesedArray = regexes.useex(saveExpression)
         print("Done")
-    
+   
     if inp == "count" :
         expArray = ArrayCommands.count(WebArray)
         print(expArray)
         print("Done")
 
+
     if inp == "TTA":
         ProssesedArray = debugcommands.TTA(ProssesedArray)
         print("Done")
+
 
     if inp == "ATT":
         debugcommands.ATT(ProssesedArray)
         print("Done")
 
+
     if inp == "findNames":
         WebArray = ArrayCommands.findNames(ProssesedArray)
         print("Done")
+
 
     if inp == "cleanWebArray":
         ArrayCommands.cleanWebArray(saveExpression, WebArray)
         print("Done")
 
+
     if inp == "export":
         ArrayCommands.exportWebArray(expArray)
 
+
     if inp == "deviation":
         print(maths.SD(expArray))
-    
+   
     if inp == "mean":
         print(maths.mean(expArray))
-    
+   
     if inp == "median":
         print(maths.median(expArray))
+
 
     if inp == "mode":
         print(maths.mode(expArray))
 
+
     if inp == "gmean":
         print(maths.gmean(expArray))
-    
+   
     if inp == "clear" :
         ## clears menu
         print("PT: clears the ProssesedText.txt document")
         print("PA: clears the ProssesedArray")
         print("ALL: clears everything")
         inp = input(":")
+
 
         if inp == "PT":
             debugcommands.PTclear
@@ -105,4 +115,9 @@ while running:
             debugcommands.PTclear
             ProssesedArray = debugcommands.PAclear
 
+
         print("Done")
+
+
+
+
